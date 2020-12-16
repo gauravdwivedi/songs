8afc55595fd50a4dbdcd12f423ec697b9600689b
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-12-16 11:48:09 +0530

    redux scaffolding

---
+++ src/reducers/index.js
@@ -0,0 +1,34 @@
+
+import {combineReducers} from 'redux'
+
+const songsReducers =()=>{
+        return [
+            {
+                title:'No Scrubs', duration:'4:05'
+            },
+            {
+                title:'Macarena', duration:'2:30'
+            },
+            {
+                title:'All Star', duration:'3:15'
+            },
+            {
+                title:'I want it that way', duration:'1:45'
+            }
+        ]
+}
+
+
+const selectedSongReducer =(selectSong=null,action)=>{
+
+    if(action.type==='SONG_SELECTED'){
+        return action.payload
+    }
+
+    return selectedSongReducer;
+}
+
+export default combineReducers({
+    songs:songsReducers,
+    selectedSong:selectedSongReducer
+})
